6150c557b9bc40a994a73c6328327329b0a769cd
Jere <jereenunez@gmail.com>
2024-09-18 19:46:23 -0300

    se agrega middleware y la funcion handle error

--- api/index.js
+++ api/index.js
@@ -1,25 +1,36 @@
-import express from 'express';
-import mongoose from 'mongoose';
+import express from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
-dotenv.config()
+dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(
-    ()=> {
-        console.log('MONGODB IS CONNECTED')
-    }
-).catch((err)=>{
-    console.log(err)
-})
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MONGODB IS CONNECTED");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.listen(3000, ()=>{
-    console.log('server is running on port 3000!!');
+app.listen(3000, () => {
+  console.log("server is running on port 3000!!");
 });
 
-app.use('/api/user',userRoutes)
-app.use('/api/auth',authRoutes)
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.use((req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
